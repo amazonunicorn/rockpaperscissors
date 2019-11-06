16c05b7f0845e16eb28652e7fe5aa9519e03b02a
Amazon Unicorn(aka Kayla) <strait750@gmail.com>
2019-11-06 23:30:59 +0000

    hello

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+
 
 //GLOBAL VARIABLES
 /* global $ */
 
+$(#shoot).click(function() {
+    $(#usersChoice).html(message);
+});
 
-
-// DOCUMENT READY FUNCTION BELOW
 
